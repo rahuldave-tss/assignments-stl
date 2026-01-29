26db6983b05ddd67f435383bec090bebd822ce83
rahuldave-tss <rahul.dave@tssconsultancy.com>
2026-01-29 12:14:32 +0530

    Updated Contents of main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Assignment-1 : Smart City Utility System (14/1/26)");
         System.out.println("Assignment-2 : Student Profile Management using OOP Concepts (20/1/26)");
+        System.out.println("Assignment-3 : Bank Management(Multiple Accounts) using OOP Concepts (27/1/26)");
+        System.out.println("Assignment-4 : Insurance Policy Management using OOP Concepts (28/1/26)");
     }
 }
